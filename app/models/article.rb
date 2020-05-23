7324173da61ad84620bb072f3cc8b0f5c8d4133c
Balakumar3018 <balakumarjkp@gmail.com>
2020-05-23 14:30:00 +0000

    crud validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
-class Article < ApplicationRecord
-    
+class Article < ApplicclearationRecord
+ validates :title, presencere:true ,length:{minimum:6, maximum:100}
+ validates :description, presencere:true ,length:{minimum:15,maximum:100}
 end
